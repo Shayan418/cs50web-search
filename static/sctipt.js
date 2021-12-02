a0ad598e6e5b0812a77b745864abf06b7bcdd941
jahanzebshayan <jahanzebshayan14@gmail.com>
2021-12-02 21:44:33 +0530

    index completed

---
+++ static/sctipt.js
@@ -0,0 +1,8 @@
+function mouseOver() {
+  document.getElementById("products-menu-circle").removeAttribute("hidden");
+
+}
+
+function mouseOut() {
+  document.getElementById("products-menu-circle").setAttribute("hidden", true);
+}
